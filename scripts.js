3d0283d9b1f5c86524975392e7865b6885325711
Zavier <zmyles@comcast.net>
2019-01-06 23:59:04 -0500

    commented code

--- scripts.js
+++ scripts.js
@@ -6,8 +6,9 @@
 window.addEventListener('keydown', function(e){
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
 
-  if(!audio) return;
+  if(!audio) return; // stops the function from running all together
+  audio.currentTime = 0; //rewinds audio to the start
 
   audio.play();
- console.log(audio);
+//  console.log(audio);
 });
