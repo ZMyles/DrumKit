a8aa7146437f96daadb99dd8fccd7f01c8bc95e1
Zavier <zmyles@comcast.net>
2019-01-28 20:00:30 -0500

    CSS animation works! on keypress, Keys on screen will flash when pressed

--- scripts.js
+++ scripts.js
@@ -3,17 +3,31 @@
 // everytime you press a key on the key board, it will gather information from what you pressed.
 
 // console logged 'e' to test it if was working in the browsers console.
-window.addEventListener('keydown', function(e){
-  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+// window.addEventListener('keydown');
 
-  if(!audio) return; // stops the function from running all together
-  audio.currentTime = 0; //rewinds audio to the start
 
-  audio.play();
-//  console.log(audio);
-});
 
-//  const key = array.from(document.querySelectorAll('.key'));
 
-//  key.forEach(key => key.addEventListener('transitionend', removeTransition));
-//  window.addEventListener('Keydown', playSound);
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+  
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+    if(!audio) return; // stops the function from running all together
+
+    key.classList.add('playing');
+    audio.currentTime = 0; //rewinds audio to the start
+    audio.play();
+    
+}
+
+function removeTransition(e) {
+  if(e.propertyName !== 'transform') return;
+    this.classList.remove('playing');
+}
+
+
+const keys = Array.from(document.querySelectorAll('.key'));
+  keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+
+
+window.addEventListener('keydown', playSound);
